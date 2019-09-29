113e55e33f13007f2944975047738fd08943e4ea
arnaud ferrand <arferrand@gmail.com>
2019-09-29 18:00:24 +0100

    adds firebase config in credentials, and update README

--- README.md
+++ README.md
@@ -3,3 +3,32 @@
 Hey friend!
 
 This is my personal todo app playground - feel free to poke around. It's built using create-react-app, Firebase, and Google Auth.
+
+## Get started
+
+\$ touch src/credentials.js
+
+edit the file with your credentials:
+
+```
+export const GOOGLE_API_APIKEY = 'YOUR_GOOGLE_API_APIKEY';
+export const GOOGLE_API_CLIENT_ID = 'YOUR_GOOGLE_API_CLIENT_ID';
+export const FIREBASE_APIKEY = 'YOUR_FIREBASE_APIKEY';
+
+export const FIREBASE_AUTHDOMAIN = 'YOUR_FIREBASE_AUTHDOMAIN';
+export const FIREBASE_DATABASEURL = 'YOUR_FIREBASE_DATABASEURL';
+export const FIREBASE_PROJECTID = 'YOUR_FIREBASE_PROJECTID';
+export const FIREBASE_STORAGEBUCKET = 'YOUR_FIREBASE_STORAGEBUCKET';
+export const FIREBASE_MESSAGINGSENDERID = 'YOUR_FIREBASE_MESSAGINGSENDERID';
+export const FIREBASE_APPID = 'YOUR_FIREBASE_APPID';
+
+```
+
+install dependencies
+\$ npm i
+
+test app render correctly
+\$ npm test
+
+run locally
+\$ npm start
--- src/FirebaseWrapper.jsx
+++ src/FirebaseWrapper.jsx
@@ -1,23 +1,34 @@
 import React, { useContext, useEffect, useMemo, useState } from 'react';
-import * as firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/firestore"
-import { useCollection, useCollectionData } from 'react-firebase-hooks/firestore'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { googleAuthContext } from "GoogleAuth"
-import { FIREBASE_APIKEY } from "./credentials"
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import {
+  useCollection,
+  useCollectionData,
+} from 'react-firebase-hooks/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { googleAuthContext } from 'GoogleAuth';
+import {
+  FIREBASE_APIKEY,
+  FIREBASE_AUTHDOMAIN,
+  FIREBASE_DATABASEURL,
+  FIREBASE_PROJECTID,
+  FIREBASE_STORAGEBUCKET,
+  FIREBASE_MESSAGINGSENDERID,
+  FIREBASE_APPID,
+} from './credentials';
 
 const firebaseConfig = {
   apiKey: FIREBASE_APIKEY,
-  authDomain: "dodo-1838e.firebaseapp.com",
-  databaseURL: "https://dodo-1838e.firebaseio.com",
-  projectId: "dodo-1838e",
-  storageBucket: "",
-  messagingSenderId: "589181506484",
-  appId: "1:589181506484:web:b50fa4a0b7c9be863298e1",
-  measurementId: "G-LJ4NS3TVPX",
-}
-firebase.initializeApp(firebaseConfig)
+  authDomain: FIREBASE_AUTHDOMAIN,
+  databaseURL: FIREBASE_DATABASEURL,
+  projectId: FIREBASE_PROJECTID,
+  storageBucket: FIREBASE_STORAGEBUCKET,
+  messagingSenderId: FIREBASE_MESSAGINGSENDERID,
+  appId: FIREBASE_APPID,
+  // measurementId: "G-LJ4NS3TVPX",
+};
+firebase.initializeApp(firebaseConfig);
 
 const tasksContext = React.createContext({
   tasks: [],
@@ -27,86 +38,102 @@ const tasksContext = React.createContext({
   tagColors: {},
   addTask: () => {},
   removeTask: () => {},
-})
-export { tasksContext }
+});
+export { tasksContext };
 
 const FirebaseWrapperWrapper = ({ children }) => {
-  const [firebaseUser, setFirebaseUser] = useState(null)
-  const { token, email } = useContext(googleAuthContext)
+  const [firebaseUser, setFirebaseUser] = useState(null);
+  const { token, email } = useContext(googleAuthContext);
 
   useEffect(() => {
     if (token) {
-      const firebaseUser = firebase.auth.GoogleAuthProvider.credential(token)
-      setFirebaseUser(firebaseUser)
+      const firebaseUser = firebase.auth.GoogleAuthProvider.credential(token);
+      setFirebaseUser(firebaseUser);
     } else {
-      firebase.auth().signOut()
-      setFirebaseUser(null)
+      firebase.auth().signOut();
+      setFirebaseUser(null);
     }
-  }, [token])
+  }, [token]);
 
-  if (!firebaseUser) return null
+  if (!firebaseUser) return null;
 
-  return (
-    <FirebaseWrapper {...{email, token, children}} />
-  )
-}
+  return <FirebaseWrapper {...{ email, token, children }} />;
+};
 
-export default FirebaseWrapperWrapper
-
+export default FirebaseWrapperWrapper;
 
 const colors = [
-  "#8e44ad", "#3dc1d3", "#833471", "#4a69bd", "#1B1464", "#c44569", "#f39c12", "#16a085", "#d35400", "#60a3bc"
-]
+  '#8e44ad',
+  '#3dc1d3',
+  '#833471',
+  '#4a69bd',
+  '#1B1464',
+  '#c44569',
+  '#f39c12',
+  '#16a085',
+  '#d35400',
+  '#60a3bc',
+];
 
 const FirebaseWrapper = ({ email, token, children }) => {
-  const tasksSnapshot = useMemo(() => email && firebase.firestore().collection(`users/${email}/tasks`), [])
-  const [tasksCollection] = useCollection(tasksSnapshot)
+  const tasksSnapshot = useMemo(
+    () => email && firebase.firestore().collection(`users/${email}/tasks`),
+    [email]
+  );
+  const [tasksCollection] = useCollection(tasksSnapshot);
   const [tasks, isLoading, didError] = useCollectionData(tasksSnapshot, {
-    idField: "id"
-  })
+    idField: 'id',
+  });
 
   const addTask = task => {
-    tasksSnapshot.add(task)
-  }
+    tasksSnapshot.add(task);
+  };
   const removeTask = task => {
-    console.log(tasksSnapshot, tasksCollection, tasksSnapshot.doc(task.id))
-    tasksSnapshot.doc(task.id).delete()
-  }
+    console.log(tasksSnapshot, tasksCollection, tasksSnapshot.doc(task.id));
+    tasksSnapshot.doc(task.id).delete();
+  };
 
   const contexts = useMemo(() => {
-    return [...new Set((tasks || []).map(task => task.context).flat(Infinity))].filter(d => d)
-  }, [tasks])
+    return [
+      ...new Set((tasks || []).map(task => task.context).flat(Infinity)),
+    ].filter(d => d);
+  }, [tasks]);
   const tags = useMemo(() => {
-    return [...new Set((tasks || []).map(task => task.tags).flat(Infinity))].filter(d => d)
-  }, [tasks])
+    return [
+      ...new Set((tasks || []).map(task => task.tags).flat(Infinity)),
+    ].filter(d => d);
+  }, [tasks]);
 
-  const contextColors = useMemo(() => (
-    Object.fromEntries(contexts.map((d, i) => [
-      d,
-      colors[i % colors.length],
-    ]))
-  ), [contexts])
+  const contextColors = useMemo(
+    () =>
+      Object.fromEntries(
+        contexts.map((d, i) => [d, colors[i % colors.length]])
+      ),
+    [contexts]
+  );
 
-  const tagColors = useMemo(() => (
-    Object.fromEntries(tags.map((d, i) => [
-      d,
-      colors[i % colors.length],
-    ]))
-  ), [tags])
+  const tagColors = useMemo(
+    () =>
+      Object.fromEntries(tags.map((d, i) => [d, colors[i % colors.length]])),
+    [tags]
+  );
 
-  const contextValue = useMemo(() => ({
-    tasks: tasks || [],
-    contexts: contexts || [],
-    tags: tags || [],
-    contextColors: contextColors || {},
-    tagColors: tagColors || {},
-    addTask,
-    removeTask,
-  }), [tasks, contexts, tags])
+  const contextValue = useMemo(
+    () => ({
+      tasks: tasks || [],
+      contexts: contexts || [],
+      tags: tags || [],
+      contextColors: contextColors || {},
+      tagColors: tagColors || {},
+      addTask,
+      removeTask,
+    }),
+    [tasks, contexts, tags, contextColors, tagColors, addTask, removeTask]
+  );
 
   return (
     <tasksContext.Provider value={contextValue}>
-      { children }
+      {children}
     </tasksContext.Provider>
-  )
-}
+  );
+};
